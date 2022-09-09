c5fe842f0c917424ce37a9a93ff451a7bb99ec9a
ezgidolma <88343326+ezgidolma@users.noreply.github.com>
2022-09-09 23:08:57 +0300

    .

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -12,6 +12,7 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded=false;
+  filterText="";
 
   constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) {}
 
